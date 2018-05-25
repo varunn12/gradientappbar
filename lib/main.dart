6d2a10a8a82102eff5596520f81402bb16fefe14
Varun Pandey <varunn.pandey@gmail.com>
2018-05-25 11:05:06 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'homepage.dart';
+
+
+void main(){
+ // Routes.initRoutes();
+  runApp(new MaterialApp(
+    title: "Gradient",
+    home: new HomePage(),
+  ));
+}
