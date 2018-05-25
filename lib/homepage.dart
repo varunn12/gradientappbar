bc7600c64ade68ba6a6d4690ff1ec980daaa10e6
Varun Pandey <varunn.pandey@gmail.com>
2018-05-25 11:09:56 +0530

    commets added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+
+// LinearGradient parameters are quite easy to understand:
+
+// colors: an array of the colors to be used in the gradient, in this case, two shades of blue.
+// begin, end: position of the first color and the last color, in this case, FractionalOffset allows us to treat the coordinates as a range from 0 to 1 both for x and y. As we want an horizontal gradient, we use same Y for both measures, and the x changes from 0.0 (left) to 1.0 (right).
+// stops: this array should have the same size than colors. It defines how the colors will distribute. [0.0, 1.0] will fill it from left to right. [0.0, 0.5] will fill the colors from left to half bar, etc.
+// tileMode: what to do if the stops do not fill the whole area. In this case, we added clamp (it will reuse the last color used), but as our gradient goes from 0.0 to 1.0, it’s not really necessary.
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -22,6 +30,7 @@ class HomePage extends StatelessWidget {
       
        @override
        Widget build(BuildContext context) {
+         //adding this so that the screen should not begin behind the status bar of the phone
          final double statusBarHeight = MediaQuery
       .of(context)
       .padding
